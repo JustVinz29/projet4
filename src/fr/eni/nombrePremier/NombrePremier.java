706fcd73e80d0f8b408e76b12f677eead4930997
Vincent BLANCHET <vblanchet2023@campus-eni.fr>
2023-09-01 10:21:20 +0200

    projet4

---
+++ src/fr/eni/nombrePremier/NombrePremier.java
@@ -0,0 +1,17 @@
+package fr.eni.nombrePremier;
+
+import java.util.Scanner;
+
+public class NombrePremier {
+
+	public static void main(String[] args) {
+		
+		String chaine = String.valueOf(10.23);
+		System.out.println("chaine : " + chaine);
+		Float.parseFloat(chaine);
+		System.out.println(chaine);
+		
+		
+	}
+}
+	
